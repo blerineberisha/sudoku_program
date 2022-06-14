3a5dd57f3709cd560b277dbd04b4da5c9ef53877
blerine <blerine.berisha@noseryoung.ch>
2022-06-14 13:24:49 +0200

    edited gitignore, added backend with maven

---
+++ sudoku_backend/src/test/java/berisha/blerine/sudoku/SudokuApplicationTests.java
@@ -0,0 +1,13 @@
+package berisha.blerine.sudoku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SudokuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
